e1317c2783ccc641930ed1b63065633fab262b6a
Sam A J McRuvie <doctorwhat35@gmail.com>
2019-08-11 17:17:31 +0100

    react router added, simple home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
+  function Home(){
+    return <h1> index </h1>; 
+  }
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Router path="/" exact component={Home} /> 
+
+
+    </Router>
   );
 }
 
