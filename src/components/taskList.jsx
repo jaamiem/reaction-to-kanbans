646ca553ca5cac9267dba4b63fca18af0fd8c94b
Jamie Muir <jamiemuir1994@gmail.com>
2019-08-12 12:42:34 +0100

    tasklist & card styling

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import TaskCard from './taskCard.jsx';
+import { ScrollPanel } from 'primereact/scrollpanel';
+import { Button } from 'primereact/button';
 
-
+const handleAddTaskClick = () => {
+  
+}
 
 const TaskList = (props) => {
 
   return (
-    <div>
-      {/* {console.log(props)} */}
-      {props.taskListState.map(task =>
-        <TaskCard key={task.id} task={task} />
-      )}
-    </div>
+    <div style={{width: '20%', }} >
+      <ScrollPanel style={{ height: '90vh', margin: 'auto' , background: '#ededed' }} >
+        {props.taskListState.map(task =>
+          <TaskCard key={task.id} task={task} />
+        )}
+      </ScrollPanel>
+      <Button 
+        onClick={handleAddTaskClick}
+        label='Add New Task' 
+        className='p-button-secondary' 
+        style={{ width: '100%', height: '3em', marginTop: '.5em' }} />
+    </div >  
   );
 }
 
-// const listStyle = {
-//   display: 'flex',
-//   flexDirection: 'column',
-//   justifyContent: 'space-between',
-//   alignItems: 'flex-start',
-// }
+const listStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+}
  
 export default TaskList;
